5bfc282b66c4850872705763b77a3e8f780ed589
Gisele <giselesouza.dev@outlook.com>
2021-07-29 14:23:23 -0300

    incluindo filtro, busca e ordenação na api

---
+++ clientes/validators.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+def cpf_valido(numero_do_cpf):
+    return len(numero_do_cpf) == 11
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(numero_do_rg):
+    return len(numero_do_rg) == 9   
